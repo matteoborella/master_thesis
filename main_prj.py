30b2e5cfbf657e75c691b10acd11a9762c63b463
matteoborella <45565558+matteoborella@users.noreply.github.com>
2018-12-06 16:49:11 +0100

    Update main_prj.py

--- main_prj.py
+++ main_prj.py
@@ -2,7 +2,6 @@ import numpy as np
 from biopandas.mol2 import PandasMol2
 import pandas as pd
 
-
 def splitter(str):
         a=str.split()
         return a
@@ -18,8 +17,6 @@ def readmol2():
 ##this is the function that take in input the path of the file we want to read and store it in fn-variable.
 ##then i use the readlines() module to read separately all the line of this file one by one, and i do this x times= number of file's lines.at the end i obtain a mol2 array where each line is stored as a string.
 
-
-
 ls=[]   #ls is the array where i'm going to store all the information splitted.then i have to convert them by columns
 
 mol=readmol2()
@@ -34,16 +31,18 @@ ind2=mol.index('@<TRIPOS>BOND\n')
 ind3=mol.index('@<TRIPOS>HEADTAIL\n')
 ind4=mol.index('@<TRIPOS>RESIDUECONNECT\n')
 ind5=mol.index('@<TRIPOS>SUBSTRUCTURE\n')
+
 ## i do this for all sections (2 of them are still missed),then I print the interested section in a separated array(name_section)
 ##sections are limitated by strings with @TRIPOS,so i just have to print the string immediatly after the beginning af the section till the beginning of the next one(ind1+1:ind2), with ind2 not considered. 
+
 atom_section=ls[ind1+1:ind2]
 bond_section=ls[ind2+1:ind5]
 headtail_section=ls[ind3+1:ind4]
 residueconnect_section=ls[ind4+1:]
 
-
 ##now i want to create different array that can store all the same type of data so i create different arrays for atom_id , atom_name ecc..
 ## i know that for each array i have in ls[], in position 0 I have atom_id, in 1 i have the name and so on. so it is easy to separate and put them in different arrays, that im going to use to create df.
+
 ##ATOM SECTION
 atom_id=[]
 atom_name=[]
@@ -56,15 +55,7 @@ subst_name=[]
 charge=[]
 
 for i in range(len(atom_section)):
-	#a=int(atom_section[i][0])
-	#b=atom_section[i][1]
-	#c=float(atom_section[i][2])
-	#d=float(atom_section[i][3])
-	#e=float(atom_section[i][4])
-	#f=atom_section[i][5]
-	#g=int(atom_section[i][6])
-	#h=atom_section[i][7]
-	#j=float(atom_section[i][8])
+
 	atom_id.append(int(atom_section[i][0]))
 	atom_name.append(atom_section[i][1])
 	x_coor.append(float(atom_section[i][2]))
@@ -75,46 +66,50 @@ for i in range(len(atom_section)):
 	subst_name.append(atom_section[i][7])
 	charge.append(float(atom_section[i][8]))
 
-
-
 ##BOND_SECTION
+
 bond_id=[]
 origin_atom_id=[]
 target_atom_id=[]
 bond_type=[]
 
 for i in range(len(bond_section)):
-	a=int(bond_section[i][0])
-	b=int(bond_section[i][1])
-	c=int(bond_section[i][2])
-	d=bond_section[i][3]
-	bond_id.append(a)
-	origin_atom_id.append(b)
-	target_atom_id.append(c)
-	bond_type.append(d)
+	
+	bond_id.append(int(bond_section[i][0]))
+	origin_atom_id.append(int(bond_section[i][1]))
+	target_atom_id.append(int(bond_section[i][2]))
+	bond_type.append(bond_section[i][3])
 
 ##HEADTAIL_SECTION
 
-name_line=[]
-res_line=[]
-for i in range(len(headtail_section)):
-	a=headtail_section[i][0]
-	b=headtail_section[i][1]
-	name_line.append(a)
-	res_line.append(b)
+head=[]
+tail=[]
+for i in range(len(atom_section)):
+	if i in range(len(headtail_section)):
+	
+		head.append(headtail_section[0][i])
+		tail.append(headtail_section[1][i])
+	else:
+		head.append('None')
+		tail.append('None')
 
-data_Atom={'Atom_id':atom_id, 'Atom_name':atom_name,'X_coor':x_coor,'Y_coor':y_coor,'Z_coor':z_coor,'Atom_type':atom_type,'Subst_id':subst_id,'Subst_name':subst_name,'Charge':charge}
-data_Bond={'Bond_id':bond_id,'Origin_atom_id':origin_atom_id,'Target_atom_id':target_atom_id,'bond_type':bond_type}
-data_Headtail={'a_name':name_line,'res_num':res_line}
+list_keys=['atom_id','atom_name','x_coor','y_coor','z_coor','atom_type','subst_id','subst_name','charge','head','tail']		
+list_values=[atom_id,atom_name,x_coor,y_coor,z_coor,atom_type,subst_id,subst_name,charge,head,tail]
+zipped=list(zip(list_keys,list_values))
+data=dict(zipped)
+df=pd.DataFrame(data)
+print(df)
 
-df_Atom=pd.DataFrame(data_Atom)
-df_Bond=pd.DataFrame(data_Bond)
-df_Headtail=pd.DataFrame(data_Headtail)
-print('Atom section:\n ',df_Atom)
-print('Bond section:\n ', df_Bond)
-print('Headtail section:\n ',df_Headtail)
+#data_Atom={'Atom_id':atom_id, 'Atom_name':atom_name,'X_coor':x_coor,'Y_coor':y_coor,'Z_coor':z_coor,'Atom_type':atom_type,'Subst_id':subst_id,'Subst_name':subst_name,'Charge':charge}
+#data_Bond={'Bond_id':bond_id,'Origin_atom_id':origin_atom_id,'Target_atom_id':target_atom_id,'bond_type':bond_type}
+#data_Headtail={'a_name':name_line,'res_num':res_line}
+#df_Atom=pd.DataFrame(data_Atom)
+#df_Bond=pd.DataFrame(data_Bond)
+#df_Headtail=pd.DataFrame(data_Headtail)
+#print('Atom section:\n ',df_Atom)
+#print('Bond section:\n ', df_Bond)
+#print('Headtail section:\n ',df_Headtail)
 #print('Residue Connect section: ', residueconnect_section)
-
 
 #print(pmol.df.head(10))
 #print(pmol.df.describe())
