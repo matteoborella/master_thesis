2f6c367bd6112567d63218f142fcfc94d3adcde2
matteoborella <45565558+matteoborella@users.noreply.github.com>
2018-12-17 15:37:37 +0100

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,5 @@
 import numpy as np
-from biopandas.mol2 import PandasMol2
+#from biopandas.mol2 import PandasMol2
 import pandas as pd
 
 
@@ -18,7 +18,7 @@ def readmol2():
 	return (mol2)
 
 ##this is the function that take in input the path of the file we want to read and store it in fn-variable.
-##then i use the readlines() module to read separately all the line of this file one by one, and i do this x times= number of file's lines.at the end i obtain a mol2 array where each line is stored as a string.
+##then i use the readlines() module to read separately all the line of this file one by one, and i do this x times= number of file's lines.at the end i obtain a mol2 array where each line is stored as a strin.
 #bond_row.append()
 
 
@@ -47,7 +47,7 @@ headtail_section=ls[ind3+1:ind4]
 residueconnect_section=ls[ind4+1:]
 
 
-##now i want to create different array that can store all the same type of data so i create different arrays for atom_id , atom_name ecc..
+##now i want to create different list that can store all the same type of data so i create different arrays for atom_id , atom_name ecc..
 ## i know that for each array i have in ls[], in position 0 I have atom_id, in 1 i have the name and so on. so it is easy to separate and put them in different arrays, that im going to use to create df.
 ##ATOM SECTION
 atom_id=[]
@@ -59,11 +59,12 @@ atom_type=[]
 subst_id=[]
 subst_name=[]
 charge=[]
-
-
+#i know that the first column of each row is the atom name so faor all the lines 
+#of atom section i have to append to atom_id list the first element
+#i do the same things for all the other columns
 
 for i in range(len(atom_section)):
-	
+
 	atom_id.append(int(atom_section[i][0]))
 	atom_name.append(atom_section[i][1])
 	x_coor.append(float(atom_section[i][2]))
@@ -74,22 +75,24 @@ for i in range(len(atom_section)):
 	subst_name.append(atom_section[i][7])
 	charge.append(float(atom_section[i][8]))
 
-
-
 ##BOND_SECTION
+np_atom_name=np.array(atom_name)
+np_atom_id=np.array(atom_id)
 bond_id=[]
 origin_atom_id=[]
 target_atom_id=[]
 bond_type=[]
 
 for i in range(len(bond_section)):
-	
+
 	bond_id.append(int(bond_section[i][0]))
 	origin_atom_id.append(int(bond_section[i][1]))
 	target_atom_id.append(int(bond_section[i][2]))
 	bond_type.append(bond_section[i][3])
 
 ##HEADTAIL_SECTION
+np_origin_atom_id=np.array(origin_atom_id)
+np_target_atom_id=np.array(target_atom_id)
 
 Head=[]
 Tail=[]
@@ -102,63 +105,96 @@ for i in range(len(atom_section)):
 	else:
 		Head.append(None)
 		Tail.append(None)
+np_head=np.array(Head)
+np_tail=np.array(Tail)
+
+
 ##BONDING ROW
-######################################################
+#########################################################################
 bond_row_head=[]
 bond_row_tail=[]
-add_row=[None for i in range(len(atom_id))]
+np_row=np.zeros(len(atom_id))
+np_row.fill(None)
+add_row=list(np_row)
+#ind=True
 
-ind=True
-ind0=True
-for i in range(len(atom_id)):
-	if atom_name[i]==Head[0]:
-		ind0=atom_id[i]
+#idnd0=True
+#for i in range(len(atom_id)):
+#	if atom_name[i]==Head[0]
+#		ind0=atom_id[i]
+#	else:
+#		ind0==False
+#	if atom_name[i]==Tail[0]:
+#		ind=atom_id[i]
+#	else:
+#		ind==False
 
-	elif atom_name[i]==Tail[0]:
-		ind=atom_id[i]
+#ind0=np_atom_id[np.where(np_atom_name==Head[0])[0]]
+arr_head=np.array(np_atom_name==Head[0])
+arr_tail=np.array(np_atom_name==Tail[0])
+ind0=np_atom_id[np.where(np_atom_name==Head[0])[0]]
+ind=np_atom_id[np.where(np_atom_name==Tail[0])[0]]
 
-	else:
-		ind=False
-		ind0=False
+#if any(arr_head)==True:
+#        ind0=np_atom_id[np.where(np_atom_name==Head[0])[0]]
+#else:
+#        ind0=np_atom_id[np.where(np_atom_name==Head[0])[0]]
+#        ind0=np.fill(None)
+#
+#if any(arr)==True:
+#	ind=np_atom_id[np.where(np_atom_name==Tail[0])[0]]
+#else:
+#	ind=np_atom_id[np.where(np_atom_name==Tail[0])[0]]
+#	ind=np.fill(None)
+#if ind0 in np_origin_atom_id:
+#	bond_row_head.append(target_atom_id[i])
+s=np.where(ind0[0]== np_origin_atom_id)[0]
+np_bond_row_head=np.array(target_atom_id[s])	
+for i in range(len(bond_id)):
 
-
-for j in range(len(bond_id)):
-
-	if origin_atom_id[j]==ind0:
-		bond_row_head.append(target_atom_id[j])
-
-	if target_atom_id[j]==ind0:
-		bond_row_head.append(origin_atom_id[j])
-
-	if origin_atom_id[j]==ind:
-		bond_row_tail.append(target_atom_id[j])
-
-	if target_atom_id[j]==ind:
-		bond_row_tail.append(origin_atom_id[j])
+	if origin_atom_id[i]==ind0:
+		bond_row_head.append(target_atom_id[i])
+	if target_atom_id[i]==ind0:
+		bond_row_head.append(origin_atom_id[i])
+	if origin_atom_id[i]==ind:
+		bond_row_tail.append(target_atom_id[i])
+	if target_atom_id[i]==ind:
+		bond_row_tail.append(origin_atom_id[i])
 	else:
 		continue
-if ind0==True:
-	add_row[ind0]=bond_row_head
-if ind==True:
-	add_row[ind]=bond_row_tail
+if any(arr_head)==True:
+	add_row[ind0[0]]=bond_row_head
+if any(arr_tail)==True:
+	add_row[ind[0]]=bond_row_tail
+#in this section i want to add an other column which is seet to None for all the lines exept for the tail and the head column. in that rows i have to put a list of atom_id bonded to them.
+#Indeed i created 2 list: one for atoms bonded to the tail and one for the head
 
-
-################################################
+#schedule:fragments can have head or tail or both, then i have to check in what case we are. the procedure is the same for both situations.
+#first i have to look at the first row of Tail where there is the atom_name of the tail part. thenm ii have to go to the Bond_section because in the second and in the third columns we have the origin atom_id and which atom is bonded to.
+#so i need the atom_id and i take it in the first part with ind and ind0
+#then i look which atom, the tail atom is bonded to and append all in the lists created.
+##############################################################
 list_keys=['atom_id','atom_name','x_coor','y_coor','z_coor','atom_type','subst_id','subst_name','charge','head','tail','add row']
 list_values=[atom_id,atom_name,x_coor,y_coor,z_coor,atom_type,subst_id,subst_name,charge,Head,Tail,add_row]
+#list_keys=['atom_id','atom_name','x_coor','y_coor','z_coor','atom_type','subst_id','subst_name','charge']
+#list_values=[atom_id,atom_name,x_coor,y_coor,z_coor,atom_type,subst_id,subst_name,charge]
 
-zipped=list(zip(list_keys,list_values))     #i found a better way to create dataframe, this way it looks more ordered
+zipped=list(zip(list_keys,list_values))
+
 data=dict(zipped)
 nodf=pd.DataFrame(data)
 df=nodf.set_index('atom_id')     # i have changed the first atomcolumn with atom_id column because they were redundant
+
 print(df)
 
 list_keys_bond=['Bond_id','Origin_atom_id','Target_atom_id','Bond_type']
 list_values_bond=[bond_id,origin_atom_id,target_atom_id,bond_type]
+
 zipped_bond=list(zip(list_keys_bond,list_values_bond))
-data_Bond=dict(zipped_bond)            #same thing for bond_section.
-nodf_bond=pd.DataFrame(data_Bond)	##I know it is not necessary to print bond section 
-df_Bond=nodf_bond.set_index('Bond_id')##but i  did it for checK
+data_Bond=dict(zipped_bond)
+
+nodf_bond=pd.DataFrame(data_Bond)
+df_Bond=nodf_bond.set_index('Bond_id')
 print(df_Bond)
 
 
@@ -171,7 +207,6 @@ print(df_Bond)
 #aname=pmol.df['atom_name']
 #xyz = pmol.df[['x', 'y', 'z']].values
 
-##CLASSES
 
 class CapFragment():
 	def __init__(self,df):
@@ -190,3 +225,4 @@ class LinkFragment():
 		self.names[0] = new_name
 	def shift(self, vector):
 		self.coords = self.coords + vector
+
